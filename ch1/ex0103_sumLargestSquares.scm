4529c9a375b9e52e2f84b39acee16fd360c406d3
mario <mariodlhk@gmail.com>
2015-04-03 21:32:22 +0200

    Update ex0103_sumLargestSquares.scm

--- ch1/ex0103_sumLargestSquares.scm
+++ ch1/ex0103_sumLargestSquares.scm
@@ -1,3 +1,7 @@
+; Chapter 1, ex03
+; Define a procedure that takes three numbers as arguments 
+; and returns the sum of the squares of the two larger numbers
+
 (define (square x) (* x x))
 
 (define (sumLargestSq a b c)
