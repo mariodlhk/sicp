9b2b03229faeaede61c49aa5af196ad1eb2b8cca
mario <mariodlhk@gmail.com>
2015-04-03 21:29:13 +0200

    Create ex0103_sumLargestSquares.scm

---
+++ ch1/ex0103_sumLargestSquares.scm
@@ -0,0 +1,16 @@
+(define (square x) (* x x))
+
+(define (sumLargestSq a b c)
+  (cond ((and (> a b) (< b c)) (+ (square a) (square c)))
+  	((and (< a b) (< a c)) (+ (square b) (square c)))
+  	(else (+ (square b) (square a)))
+	)
+  )
+
+; Test:
+(sumLargestSq 1 2 3)
+(sumLargestSq 1 3 2)
+(sumLargestSq 2 1 3)
+(sumLargestSq 2 3 1)
+(sumLargestSq 3 1 2)
+(sumLargestSq 3 2 1)
